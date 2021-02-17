e8f55189337d00481fe412633bd691ee044cdd27
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-02-17 13:01:21 +0000

    chore(cicd): deploy improvements [ci skip]

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ if [ "$CIRCLE_BRANCH" = "staging" ]; then
   SSH_HOST=$SSH_HOST_STG
 fi
 
-scp -o StrictHostKeyChecking=no ./docker-compose.yml $SSH_USER@$SSH_HOST_DEV:$PROJECT_DIR
+scp -o StrictHostKeyChecking=no ./docker-compose.yaml $SSH_USER@$SSH_HOST_DEV:$PROJECT_DIR
 
 ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST_DEV "cd $PROJECT_DIR && docker-compose --env-file .env pull && docker-compose --env-file .env up -d"
