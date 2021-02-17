aec4d632fc16f24a66de5ae7a9dc14e813ed6337
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-02-17 12:57:11 +0000

    chore(cicd): deploy improvements

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+PROJECT_DIR="/apps/apisuite-be"
+
 SSH_HOST=$SSH_HOST_DEV
 if [ "$CIRCLE_BRANCH" = "staging" ]; then
   SSH_HOST=$SSH_HOST_STG
 fi
 
-scp -o StrictHostKeyChecking=no ./docker-compose.yml $SSH_USER@$SSH_HOST_DEV:~/apisuite-be
+scp -o StrictHostKeyChecking=no ./docker-compose.yml $SSH_USER@$SSH_HOST_DEV:$PROJECT_DIR
 
-ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST_DEV "cd apisuite-be && docker-compose --env-file .env pull && docker-compose --env-file .env up -d"
+ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST_DEV "cd $PROJECT_DIR && docker-compose --env-file .env pull && docker-compose --env-file .env up -d"
