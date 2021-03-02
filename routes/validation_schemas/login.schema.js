8f206203560bc4b9450e8e6cc08357d027f467fe
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-03-02 19:34:20 +0000

    test(validations): login validation schemas

--- routes/validation_schemas/login.schema.js
+++ routes/validation_schemas/login.schema.js
@@ -4,14 +4,10 @@ const Joi = require('joi')
 const loginSchema = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().required(),
-}).options({
-  allowUnknown: true,
 })
 
 const lastLoginSchema = Joi.object({
   user_id: Joi.number().integer().required(),
-}).options({
-  allowUnknown: true,
 })
 
 module.exports = {
