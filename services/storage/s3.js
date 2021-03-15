f779fef75784c0ce72939df9c4dda773425c8c6e
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-03-15 12:06:44 +0000

    feat(storage): upload file stream; return public URL

--- services/storage/s3.js
+++ services/storage/s3.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const Storage = require('./storage')
 const log = require('../../util/logger')
 const {
@@ -7,26 +8,33 @@ const {
 
 class S3 extends Storage {
   async saveFile (filePath, name) {
+    const errorObject = {
+      objectURL: null,
+      error: new Error('failed to upload file to storage'),
+    }
     const client = new S3Client(this.config)
+
+    const fileStream = fs.createReadStream(filePath)
+    fileStream.on('error', (error) => {
+      log.error(error, '[S3 READ STREAM]')
+      return errorObject
+    })
 
     const cmd = new PutObjectCommand({
       Bucket: this.config.bucket,
       Key: name,
-      Body: filePath,
+      Body: fileStream,
     })
 
     try {
       await client.send(cmd)
     } catch (error) {
-      log.error('[S3 PUT OBJECT] ', error)
-      return {
-        objectURL: null,
-        error: new Error('failed to upload file to storage'),
-      }
+      log.error(error, '[S3 PUT OBJECT]')
+      return errorObject
     }
 
     return {
-      objectURL: `s3://${this.config.bucket}/${name}`,
+      objectURL: `https://${this.config.bucket}.s3-${this.config.region}.amazonaws.com/${name}`,
       error: null,
     }
   }
