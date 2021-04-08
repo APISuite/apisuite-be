a5bb347b5824ba1601f43b8cc6a9c5518079edc0
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-04-08 12:34:04 +0100

    feat: changes to default portal settings

--- util/portal-settings.js
+++ util/portal-settings.js
@@ -1,87 +1,67 @@
 module.exports = {
-  includes: {
-    alert: false,
-    demo: true,
-    instance: false,
-    console: false,
-  },
   portalName: 'API Suite Portal',
   clientName: 'API Suite',
-  infra: {
-    hydra: 'hydraauth.apisuite.io',
-    sandbox: 'sandbox.apisuite.io',
-    remoteAPI: 'remoteAPI',
-  },
-  social: {
-    web: 'https://apisuite.io/',
-    twitter: 'https://twitter.com/theapisuite',
-    github: 'https://github.com/APISuite',
-  },
-  footer: {
-    copyright: '© APISuite 2021.\nAll rights reserved.\nProudly made in Europe.',
-  },
-  i18n: [
-    {
-      locale: 'en-US',
-      label: 'We speak English',
-    },
-    {
-      locale: 'pt-PT',
-      label: 'Nós falamos Português',
-    },
+  supportURL: 'https://intercom.help/api-suite/en/articles/4586659-api-portal-users',
+  documentationURL: '',
+  socialURLs: [],
+  i18nOptions: [
+    { locale: 'en-US', label: 'We speak English' },
+    { locale: 'pt-PT', label: 'Nós falamos Português' },
   ],
-  palette: {
-    background: {
-      default: '#FFFFFF',
+  theme: {
+    type: 'light',
+    common: { black: '#000000', white: '#FFFFFF' },
+    primary: { main: '#14283C', dark: '#000017', light: '#374858', contrastText: '#FFFFFF' },
+    secondary: { main: '#32C896', dark: '#007C45', light: '#B3E7D1', contrastText: '#14283C' },
+    error: { main: '#FF1515', dark: '#840608', light: '#FFA1A1' },
+    warning: { main: '#F78E27', dark: '#80460B', light: '#FFDCB9' },
+    info: { main: '#19B3EE', dark: '#035E86', light: '#BBECFF' },
+    success: { main: '#14DE2D', dark: '#037C17', light: '#A9F19E' },
+    gradient: {
+      main: 'linear-gradient(270deg, #C8DC8C 0%, #007D7D 100%)',
+      dark: 'linear-gradient(270deg, #7DD291 0%, #007D7D 100%)',
+      light: 'linear-gradient(270deg, #C8DC8C 0%, #19A38A 100%)',
     },
-    primary: '#32C896',
-    primaryContrastText: '#FFFFFF',
-    secondary: '#19B3EE',
-    secondaryContrastText: '#FFFFFF',
-    tertiary: '#14283C',
-    tertiaryContrastText: '#FFFFFF',
-    active: '#51606E',
-    error: '#FF1515',
-    focus: '#19B3EE',
-    info: '#19B3EE',
-    label: '#BAC0C6',
-    success: '#14DE2D',
-    warning: '#F78E27',
-    greyScales: {
-      50: '#EEEEEE',
-      100: '#E3E3E3',
-      300: '#D1D1D1',
-      400: '#ACACAC',
-      500: '#8B8B8B',
-      600: '#646464',
-      700: '#646464',
-      800: '#515151',
-      900: '#131313',
-    },
-    newGreyScales: {
-      25: '#F7F8F9',
-      50: '#ECEDEF',
-      100: '#DBDEE1',
+    grey: {
+      50: '#F7F8F9',
+      100: '#ECEDEF',
+      200: '#DBDEE1',
       300: '#BAC0C6',
       400: '#85909A',
       500: '#6A7783',
-      700: '#374858',
+      600: '#646464',
+      700: '#535E68',
+      800: '#515151',
+      900: '#131313',
     },
     text: {
-      primary: '#333333',
-      secondary: '#424242',
+      primary: '#14283C',
+      secondary: '#85909A',
+      disabled: '#DBDEE1',
+      hint: '#51606E',
     },
-    feedback: {
-      error: '#FF0000',
+    divider: '#DBDEE1',
+    action: {
+      active: '#51606E',
+      hover: 'rgba(0,0,0,0.04)',
+      selected: 'rgba(0,0,0,0.08)',
+      disabled: '#DBDEE1',
+      disabledBackground: 'rgba(0, 0, 0, 0.12)',
+      focus: '#19B3EE',
     },
-    alert: {
-      success: {
-        background: '#BBECFF',
-      },
+    background: {
+      default: '#F7F8F9',
+      paper: '#FFFFFF',
+    },
+    label: '#BAC0C6',
+    dimensions: {
+      borderRadius: 4,
     },
   },
-  dimensions: {
-    borderRadius: 4,
+  infra: {
+    hydra: 'hydraauth.develop.apisuite.io',
+    sandbox: 'sandbox.develop.apisuite.io',
+    remoteAPI: 'remoteAPI',
   },
   pages: {
     landing: {
