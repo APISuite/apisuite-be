5e37c6ea10035527e9e7e3c408c6bed3c18fa255
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-05-21 14:31:15 +0100

    refactor: spliting app media sub-routes and controllers

--- controllers/app.js
+++ controllers/app.js
@@ -15,6 +15,7 @@ const {
 } = require('../util/enums')
 const Idp = require('../services/idp')
 const publicAppsController = require('./app.public')
+const appMediaController = require('./app.media')
 
 const appAttributes = {
   include: [
@@ -540,104 +541,6 @@ const isSubscribedTo = async (req, res) => {
   }
 }
 
-const uploadMedia = async (req, res) => {
-  if (!req.formdata || !req.formdata.files) {
-    return res.status(HTTPStatus.BAD_REQUEST).send({ errors: ['no files uploaded'] })
-  }
-
-  const files = []
-  const badTypes = []
-  for (const key in req.formdata.files) {
-    const file = req.formdata.files[key]
-    if (file.type.split('/')[0] !== 'image') {
-      badTypes.push(file.name)
-    }
-    files.push(file)
-  }
-
-  if (badTypes.length) {
-    return res.status(HTTPStatus.BAD_REQUEST).send({
-      errors: badTypes.map((f) => ({
-        file: f,
-        error: 'invalid type (image expected)',
-      })),
-    })
-  }
-
-  const app = await models.App.findOne({
-    where: {
-      id: req.params.id,
-      org_id: req.user.org.id,
-      enable: true,
-    },
-  })
-
-  if (!app) return res.status(HTTPStatus.NOT_FOUND).send({ errors: ['App not found'] })
-
-  const storageClient = Storage.getStorageClient()
-  const savePromises = files.map((f) => {
-    const extension = f.name.split('.').pop()
-    return storageClient.saveFile(f.path, `app-media-${req.params.id}-${uuidv4()}.${extension}`)
-  })
-  const saveResults = await Promise.all(savePromises)
-
-  try {
-    await Promise.all(files.map((f) => fs.unlink(f.path)))
-  } catch (err) {
-    log.error(err, 'uploadMedia: failed to remove temporary files')
-  }
-
-  const response = {
-    savedImages: [],
-    errors: [],
-  }
-  const imageURLs = []
-
-  for (let i = 0; i < saveResults.length; i++) {
-    const sr = saveResults[i]
-    if (sr.objectURL && sr.objectURL.length) {
-      imageURLs.push(sr.objectURL)
-      response.savedImages.push({
-        file: files[i].name,
-        url: sr.objectURL,
-      })
-      continue
-    }
-
-    response.errors.push({
-      file: files[i].name,
-      error: 'failed to save image',
-    })
-  }
-
-  app.images = app.images.concat(imageURLs)
-  await app.save()
-
-  if (!response.errors.length) delete response.errors
-
-  return res.status(HTTPStatus.OK).send(response)
-}
-
-const deleteMedia = async (req, res) => {
-  const app = await models.App.findOne({
-    where: {
-      id: req.params.id,
-      org_id: req.user.org.id,
-      enable: true,
-    },
-  })
-
-  if (!app) return res.status(HTTPStatus.NOT_FOUND).send({ errors: ['App not found'] })
-
-  const storageClient = Storage.getStorageClient()
-  await Promise.all(req.body.images.map((img) => storageClient.deleteFile(img)))
-
-  app.images = app.images.filter((img) => !req.body.images.includes(img))
-  await app.save()
-
-  return res.sendStatus(HTTPStatus.NO_CONTENT)
-}
-
 const patchApp = async (req, res) => {
   const transaction = await sequelize.transaction()
   try {
@@ -716,8 +619,7 @@ module.exports = {
   subscribeToAPI,
   listApps,
   isSubscribedTo,
-  uploadMedia,
-  deleteMedia,
   patchApp,
   ...publicAppsController,
+  ...appMediaController,
 }
---
+++ routes/app.media.js
@@ -0,0 +1,108 @@
+const { decorateRouter } = require('@awaitjs/express')
+const router = decorateRouter(require('express').Router())
+const controllers = require('../controllers')
+const { actions, possessions, resources } = require('../util/enums')
+const { accessControl, loggedIn, fileParser } = require('../middleware')
+const { deleteMediaBody } = require('./validation_schemas/app.schema')
+
+/**
+ * @openapi
+ * /apps/:id/media:
+ *   put:
+ *     summary: Upload app images/media
+ *     tags: [App]
+ *     security:
+ *       - cookieAuth: []
+ *     requestBody:
+ *       description: App object that need to be created
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               filename:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                   format: binary
+ *     responses:
+ *       200:
+ *         description: Upload results
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 savedImages:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       file:
+ *                         type: string
+ *                       url:
+ *                         type: string
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       file:
+ *                         type: string
+ *                       error:
+ *                         type: string
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       403:
+ *         $ref: '#/components/responses/Forbidden'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ */
+router.putAsync('/',
+  loggedIn,
+  accessControl(actions.UPDATE, possessions.OWN, resources.APP, { idCarrier: 'params', idField: 'id' }),
+  fileParser,
+  controllers.app.uploadMedia)
+
+/**
+ * @openapi
+ * /apps/:id/media:
+ *   delete:
+ *     summary: Delete selected media objects
+ *     tags: [App]
+ *     security:
+ *       - cookieAuth: []
+ *     requestBody:
+ *       description: Media objects to delete
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               images:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *     responses:
+ *       204:
+ *         description: No content
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       403:
+ *         $ref: '#/components/responses/Forbidden'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ */
+router.deleteAsync('/',
+  loggedIn,
+  deleteMediaBody,
+  accessControl(actions.UPDATE, possessions.OWN, resources.APP, { idCarrier: 'params', idField: 'id' }),
+  controllers.app.deleteMedia)
+
+module.exports = router
