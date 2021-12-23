a8d76fe48b00b223848ff79950333acd6f13067f
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-12-23 10:11:22 +0000

    feat: handling for 413 errors

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,12 @@
 const log = require('../util/logger')
 const { v4: uuidv4 } = require('uuid')
+const HTTPStatus = require('http-status-codes')
 
 module.exports = (err, req, res, next) => {
+  if (err?.status === 413) {
+    return res.status(HTTPStatus.REQUEST_TOO_LONG).send({ errors: ['Request payload too large'] })
+  }
+
   const errorId = uuidv4()
   log.error({ err, error_id: errorId })
   res.sendInternalError(`Something went wrong while handling your request. Here's an error reference in case you contact support: ${errorId}`)
