30db33c4198644822820842ecfb4c66af84792ec
Joao-Castilho <90632638+Joao-Castilho@users.noreply.github.com>
2022-05-06 16:10:00 +0100

    fix: plan validation (#85)

--- middleware/plan-control.js
+++ middleware/plan-control.js
@@ -8,11 +8,12 @@ const planControl = async (req, res) => {
     const plan = await models.Plan.findAll()
 
     if (plan.length === 0) {
-      res.loggedInUser.type = 'full'
+      req.user.type = 'full'
+      req.user.plan = {}
+    } else {
+      req.user.type = plan[0].dataValues.type
+      req.user.plan = plan[0].dataValues.plan
     }
-
-    req.user.type = plan[0].dataValues.type
-    req.user.plan = plan[0].dataValues.plan
   } catch (err) {
     return res.status(HTTPStatus.BAD_REQUEST).json({ errors: ['ERROR GETTING PLAN'] })
   }
