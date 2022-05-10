086e4f48ac42481178a9ef140ca7d60c3a39b6d8
cloudokitroni <100689005+cloudokitroni@users.noreply.github.com>
2022-05-10 15:14:03 +0100

    Fix/migration fix (#89)
    * fix: fixes migration
    
    * fix: fixes migration

--- migrations/20220510122118-adds-default_resources.js
+++ migrations/20220510122118-adds-default_resources.js
@@ -7,13 +7,13 @@ module.exports = {
     try {
       const organization = await models.Organization.getOwnerOrganization()
       const recordsToAdd = [
-        { orgId: organization.id, url: `${config.get('apiURL')}/media/space-background.svg`, namespace: 'marketplace.background', language: 'en-US' },
-        { orgId: organization.id, url: `${config.get('apiURL')}/media/marketplace.svg`, namespace: 'marketplace.hero', language: 'en-US' },
-        { orgId: organization.id, url: `${config.get('apiURL')}/media/marketplaceApps.svg`, namespace: 'marketplace.apps', language: 'en-US' },
+        { org_id: organization.id, url: `${config.get('apiURL')}/media/space-background.svg`, namespace: 'marketplace.background', language: 'en-US' },
+        { org_id: organization.id, url: `${config.get('apiURL')}/media/marketplace.svg`, namespace: 'marketplace.hero', language: 'en-US' },
+        { org_id: organization.id, url: `${config.get('apiURL')}/media/marketplaceApps.svg`, namespace: 'marketplace.apps', language: 'en-US' },
       ]
 
       for (const record of recordsToAdd) {
-        const resourceExists = await models.Resource.findByNamespace(record.orgId, record.namespace, record.language)
+        const resourceExists = await models.Resource.findByNamespace(record.org_id, record.namespace, record.language)
         if (!resourceExists) {
           await queryInterface.insert(null, 'resource', record)
         }
